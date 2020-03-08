3aad9433b7909be20a592f30142130557335444f
hhbgk <hhbjsj@163.com>
2020-03-08 18:34:02 +0800

    Improve

---
+++ DesignPatternKit/src/main/java/com/hhbgk/designpatternkit/MyClass.java
@@ -0,0 +1,9 @@
+package com.hhbgk.designpatternkit;
+
+public class MyClass {
+//    private static String tag = Client.class.getSimpleName();
+    public static void main(String[] args) {
+
+        System.out.println("My name is longyoung");
+    }
+}
